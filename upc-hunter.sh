97bd4ef84953e36b3f4e9940047cafbcfbafa5e5
Kartik Singh <kartiksingh00013@gmail.com>
2021-11-26 04:42:06 +0530

    Create upc-hunter.sh

---
+++ upc-hunter.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+banner='U.C.P-Hunter'
+
+developer='Developer: https://github.com/kartik00013\n'
+
+figlet $banner
+echo -e $developer
+
+file=$1
+
+if [ "$#" = 0 ]; then
+        echo -e 'Usage: bash upc-hunter.sh filename\n'
+        exit
+else
+        if [ -r "$file" ]; then
+                echo -e 'Working on it......\n'
+                while read target; do 
+                        if [[ $(curl -s -X POST "https://$target" | grep '“status”: “ok”' > /dev/null) ]]; then
+                                echo $target
+                        fi
+                done < $file
+        else
+                echo 'Error: Either the file does not exist at the specified path or the file does not have read permission'
+                exit
+        fi
+fi
